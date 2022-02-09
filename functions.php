dd1d20db283fae9a3eab5f99ccc5a17015ebbfc8
turston-l <louwturston@gmail.com>
2022-02-09 13:52:23 +0200

    added functions

--- functions.php
+++ functions.php
@@ -12,4 +12,34 @@ function pdo_connect_mysql() {
     	// If there is an error with the connection, stop the script and output the error.
     	exit('Failed to connect to database!');
     }
-}
+    // Template header; feel free to customize it, but do not indent the PHP code or it will throw an error
+function template_header($title) {
+    echo <<<EOT
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>$title</title>
+            <link href="style.css" rel="stylesheet" type="text/css">
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+        </head>
+        <body>
+        <nav class="navtop">
+            <div>
+                <h1>Gallery System</h1>
+                <a href="index.php"><i class="fas fa-image"></i>Gallery</a>
+            </div>
+        </nav>
+    EOT;
+    }
+    
+
+// Template footer
+function template_footer() {
+    echo <<<EOT
+        </body>
+    </html>
+    EOT;
+    }
+?>
+
