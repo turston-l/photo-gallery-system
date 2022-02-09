1e866428ac7b47002df729604f1ceb8222807b32
turston-l <louwturston@gmail.com>
2022-02-09 13:48:10 +0200

    creating the functions file

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+function pdo_connect_mysql() {
+    // The below variables should reflect your MySQL credentials
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = '';
+    $DATABASE_NAME = 'phpgallery';
+    try {
+        // Connect to MySQL using the PDO extension
+    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+    	// If there is an error with the connection, stop the script and output the error.
+    	exit('Failed to connect to database!');
+    }
+}
