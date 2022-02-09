78b01359cd9014063590118c91aad62864eff081
turston-l <louwturston@gmail.com>
2022-02-09 13:53:38 +0200

    creating the home page

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include 'functions.php';
+// Connect to MySQL
+$pdo = pdo_connect_mysql();
+// MySQL query that selects all the images
+$stmt = $pdo->query('SELECT * FROM images ORDER BY uploaded_date DESC');
+$images = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
